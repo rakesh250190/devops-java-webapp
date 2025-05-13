57acee3ffa26c4c85b49213fab5c8e00ee9a6c8c
rakesh250190 <71925807+rakesh250190@users.noreply.github.com>
2025-05-14 00:09:16 +0530

    Create ApplicationStop.sh

---
+++ ApplicationStop.sh
@@ -0,0 +1,10 @@
+echo "shutting down tomcat service"
+sh /home/ec2-user/apache-tomcat-11.0.7/bin/shutdown.sh
+ps -ef|grep tomcat|grep -v grep
+if [ $? -eq 0 ]
+then
+echo "service did not stop"
+exit 1
+else
+echo "tomcat services stopped"
+fi
